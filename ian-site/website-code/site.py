8d649b7f61bb2424ff53aef2bd64f1a2f657e836
Ian Roberts <iar31.roberts@gmail.com>
2020-07-21 19:33:15 +0100

    updating python app

--- ian-site/website-code/site.py
+++ ian-site/website-code/site.py
@@ -3,10 +3,10 @@ from flask import Flask
 app = Flask(_name_)
 
 
-@app.route("/")
-def home():
-    return "This if my first Python website"
+# @app.route("/")
+# def home():
+#     return "This if my first Python website"
 
 
-if _name_ == "_main_":
+if _name_ == '_main_':
     app.run()
