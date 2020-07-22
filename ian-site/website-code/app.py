792ac1a14e1589f472ceb20ae33121c07b322f1f
Ian Roberts <iar31.roberts@gmail.com>
2020-07-22 08:46:27 +0100

    adding extra routes to app

--- ian-site/website-code/app.py
+++ ian-site/website-code/app.py
@@ -8,5 +8,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/articles')
+def articles():
+    return render_template('articles.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
