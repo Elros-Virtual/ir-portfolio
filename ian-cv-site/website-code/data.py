e2a9eee7d26372f993584d6ed66b9672ba192c74
Ian Roberts <iar31.roberts@gmail.com>
2020-11-16 15:04:28 +0000

    add articles page and data

---
+++ ian-cv-site/website-code/data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article one',
+            'body': 'This is the body of the Article',
+            'author': 'Jeff Bezoz',
+            'create_data': '16-11-2020'
+        },
+        {
+            'id': 2,
+            'title': 'Article two',
+            'body': 'This is the body of the Article',
+            'author': 'Jeff Bezoz',
+            'create_data': '16-11-2020'
+        }
+    ]
+    return articles
