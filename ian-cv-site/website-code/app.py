4451765ec964fb2f2a369a5779f5173b45efe09f
Ian Roberts <iar31.roberts@gmail.com>
2021-05-30 13:42:37 +0100

    Learning on portfolio site logins/blueprints/db

--- ian-cv-site/website-code/app.py
+++ ian-cv-site/website-code/app.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, flash
+from flask.helpers import url_for
+from werkzeug.utils import redirect
+from datetime import timedelta
 from data import Articles
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "hello"
+app.permanent_session_lifetime = timedelta(days=5)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
+app.config["SWLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+
+class users(db.Model):
+    _id = db.Column("id", db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
 
 Articles = Articles()
 
@@ -16,6 +37,59 @@ def about():
     return render_template('about.html')
 
 
+@app.route('/login', methods=["POST", "GET"])
+def login():
+    if request.method == "POST":
+        session.permanent - True
+        user = request.form["name"]
+        session["user"] = user
+
+        found_user = users.query.filter_by(name=user).first()
+        if found_user:
+            session["email"] = found_user.email
+        else:
+            usr = users(user, "")
+            db.session.add(usr)
+            db.session.commit()
+
+        flash("Login Succesful!")
+        return redirect(url_for("user"))
+    else:
+        if "user" in session:
+            flash("Already logged in")
+            return redirect(url_for("user"))
+        return render_template('login.html')
+
+
+@app.route("/logout")
+def logout():
+    flash("You have been logged out!", "info")
+    session.pop("user", None)
+    session.pop("email", None)
+    return redirect(url_for("login"))
+
+
+@app.route("/user", methods=["POST", "GET"])
+def user():
+    email = None
+    if "user" in session:
+        user = session["user"]
+        if request.method == "POST":
+            email = request.form["email"]
+            session["email"] = email
+            found_user = users.query.filter_by(name=user).first()
+            found_user.email = email
+            db.session.commit()
+            flash("Email added")
+        else:
+            if "email" in session:
+                email = session["email"]
+        return render_template("user.html", email=email)
+    else:
+        "You are not logged in!"
+        return redirect(url_for("login"))
+
+
 @app.route('/articles')
 def articles():
     return render_template('articles.html', articles=Articles)
@@ -26,5 +100,12 @@ def article(id):
     return render_template('article.html', id=id)
 
 
+@app.route("/db")
+def database():
+    return render_template("db.html", values=users.query.all())
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    db.create_all()
+    #app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
